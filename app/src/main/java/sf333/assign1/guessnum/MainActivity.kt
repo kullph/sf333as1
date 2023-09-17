864d9b6b9d2723f6c251c0d79da728100f76b554
kullph <kullaphat01.tr@gmail.com>
2023-09-17 21:32:22 +0700

    ฟ้าแลบ

--- app/src/main/java/sf333/assign1/guessnum/MainActivity.kt
+++ app/src/main/java/sf333/assign1/guessnum/MainActivity.kt
@@ -1,29 +1,25 @@
 package sf333.assign1.guessnum
 
 import android.os.Bundle
-import android.widget.Button
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import sf333.assign1.guessnum.ui.theme.GuessNumTheme
 
+var num: String = "0"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +28,40 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Column(
                     modifier = Modifier
-                        .fillMaxSize()
+                        .fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
                 ) {
                     Header("Guess the number!")
-                    GuessButton("GUESS")
+
+                    Row {
+                        NumberTxt()
+                    }
+
+                    Row {
+                        NumButton("1")
+                        NumButton("2")
+                        NumButton("3")
+                    }
+
+                    Row {
+                        NumButton("4")
+                        NumButton("5")
+                        NumButton("6")
+                    }
+
+                    Row {
+                        NumButton("7")
+                        NumButton("8")
+                        NumButton("9")
+                    }
+
+                    Row {
+                        NumButton("0")
+                        GuessButton("GUESS")
+
+                    }
+
                 }
             }
         }
@@ -46,15 +72,31 @@ class MainActivity : ComponentActivity() {
 fun Header(text1: String, modifier: Modifier = Modifier) {
     Text(
         text = "$text1",
-        modifier = modifier
+        modifier = modifier,
     )
 }
 
 @Composable
-fun NumberField(modifier: Modifier = Modifier) {
-    TextField(
-        value = "a"
+fun NumberTxt(, modifier: Modifier = Modifier) {
+    Text(
+        text = num,
+        modifier = modifier,
     )
+}
+
+@Composable
+fun NumButton(text1: String, modifier: Modifier = Modifier){
+    Button(
+        onClick = { num += text1 },
+        modifier = modifier
+            .width(100.dp)
+            .height(100.dp)
+            .padding(5.dp)
+    )
+    {
+        Text(text1)
+    }
+
 }
 
 @Composable
@@ -62,8 +104,9 @@ fun GuessButton(text1: String, modifier: Modifier = Modifier){
     Button(
         onClick = { },
         modifier = modifier
-            .width(240.dp)
-            .height(80.dp)
+            .width(200.dp)
+            .height(100.dp)
+            .padding(5.dp)
     )
     {
         Text(text1)
